3cf8890f07d01aa2d246bf55297f40d42dc4c771
Kseniya Segen <kseniyasegen@yandex.ru>
2022-05-24 12:15:42 +0300

    два теста

---
+++ src/main/java/Endpoints.java
@@ -0,0 +1,9 @@
+public class Endpoints {
+
+   // public static final String URL = "https://api2.stage.noomera.ru";
+    public static final String URL = "https://api2.rke.dev.noomera.ru";
+    public static final String AUTHENTICAUTION_SEND_CODE_URL = URL +"/v2/authentication/send_code";
+    public static final String AUTHENTICAUTION_AUTHENTICATE_URL = URL +"/v2/authentication/authenticate";
+    public static final String ORDER_URL = "/orders";
+    public static final String ORDER_CANCEL = "/orders/cancel";
+}
