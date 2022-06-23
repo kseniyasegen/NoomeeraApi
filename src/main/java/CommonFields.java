606707f718c084e9209b2265d6347f8f102531bb
Kseniya Segen <kseniyasegen@yandex.ru>
2022-06-23 11:03:10 +0300

    готовые тесты на users

--- src/main/java/CommonFields.java
+++ src/main/java/CommonFields.java
@@ -1,2 +1,24 @@
 public class CommonFields {
+
+    public static final String email = "nik1@testmail.test";
+    public static final String code_verifier = "axnkpyjwpsxx";
+    public static final int user_id = 3041293;
+    public static final String user_type = "UserSimple";
+    public static final String version = "4.44";
+    public static final int limit = 20;
+    public static final int offset = 0;
+    public static final int state = 2;
+    public static final String uniqname = "nikfirstchat32";
+    public static final String source = "4535343534";
+    //комментарий при шаринге профиля
+    public static final String comment = "Через автотест";
+    //id юзеров, которым будет шариться профиль
+    public static final int user_ids = 3041066;
+    //id юзера, чей профиль будет шариться
+    public static final int shareProfileId = 3041293;
+    //id юзера, чей профиль будет запрашиваться
+    public static final int usersProfileId = 3041293;
+    //id юзера, ссылка на чей профиль будет запрашиваться
+    public static final int  userLinkId = 3041293;
+
 }
