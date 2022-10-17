a16d86640698487fbbeefe09c3abb6a81ccf7780
kseniyasegen <kseniyasegen@yandex.ru>
2022-10-17 23:24:12 +0300

    17 10 2022

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -2,12 +2,14 @@ import static io.restassured.RestAssured.given;
 
 public class Map {
 
-    public void postAddEventOnMap() {
+  /*  public void postAddEventOnMap() {
         CommonFields.response = given().log().all()
                 .queryParam("gps_x", CommonFields.addGroupName)
                 .queryParam("gps_y", CommonFields.addGroupDescription)
-                .queryParam("royalty", 0)
-                .queryParam("image", "")
+                .queryParam("user_gps_x", 0)
+                .queryParam("user_gps_y", 0)
+                .queryParam("type_id", 0)
+                .queryParam("text", "")
                 .baseUri(Endpoints.URL)
                 .header("Content-type", "application/json")
                 .header("n-build", "4.43.0")
@@ -19,4 +21,6 @@ public class Map {
                 .post(Endpoints.POST_ADD_EVENT_ON_MAP_URL);
     }
 
+
+   */
 }
